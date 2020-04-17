d14d716790762979baf1d2455a548751eae5de89
zguang <zguang.loh@gmail.com>
2020-04-18 03:08:21 +0800

    Advanced Templating

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs"); // start hbs
 app.set("views", viewsPath); // changing hbs default views folder to another directory
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -23,7 +26,11 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", { title: "Help", helpText: "This is a helpful text" });
+  res.render("help", {
+    title: "Help",
+    name: "Jason",
+    helpText: "This is a helpful text",
+  });
 });
 
 app.get("/weather", (req, res) => {
